afaded9fe8f9f7608102fe40146e5d0e71b76907
McCambley <jakemccamb16@gmail.com>
2025-08-23 21:51:07 -0400

    feat: update representatives section with markdown conversion and improved structure

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// 4. JavaScript to perform the conversion
-window.onload = function () {
-  // Get the markdown text from the script tag
+const markdown = `
+# Jackson Representatives
 
-  // Convert the markdown text to HTML
-  const html = marked.parse(`
 Find information below about your federal and state representatives, including their names, party affiliations, and maps of their districts.
 
-## Federal
+## Representatives at the Federal Level
 
 This section details people that represent you in the federal government.
 
-### Congress
+### Congress - US House and Senate
 
 This section includes your representatives in the US Congress. There are two chambers in Congress: the House of Representatives and the Senate. New Hampshire has two representatives in the House and two senators in the Senate.
 
@@ -18,68 +15,78 @@ This section includes your representatives in the US Congress. There are two cha
 
 The House of Representatives has 435 members, apportioned by population. New Hampshire has two representatives.
 
-**Names**
+##### District 2
 
-- (District 2) Maggie Goodlander (D)
-    - New Hampshire District #2
-- (Distrcit 1) Not Chris Pappas (D)
+- Maggie Goodlander (D)
+
+##### District 1 
+
+- Chris Pappas (D)
+
+_**Note:** Jackson is in District 2, so Chris Pappas is not our representative. He does represent our neighbors and interests in Bartlett, Hart's Location, Chatham, and North Conway, however, so it's worth noting his presence and politics._
 
 
 #### US Senate
 
 The Senate has 100 members, two from each state.
 
-**Names**
-
 - Maggie Hassan (D)
 - Jeanee Shaheen (D)
 
-**Maps**
+**Map of Congressional Districts in New Hampshire**
 
 ![Congressional Districts](/assets/cong_dist.png)
 
-## State
+## Representatives at the State Level
 
-### Governor (1)
-
-**Name**
+### Governor
 
 - Kelly Ayotte (R)
 
-**Map**
-
-![Town & County](/assets/town_count.png)
-
 ### General Court
 
-#### State House (400)
+The New Hampshire General Court is the state's legislative body. It is bicameral, meaning it has two chambers: the House of Representatives and the Senate. Think of it like the US Congress, but for New Hampshire.
 
-**District 2**
+#### State House
+
+The New Hampshire House of Representatives has 400 members, apportioned by population. Jackson is in District 2 of Carroll County, which has two representatives.
+
+##### District 2
 
 - [Anita D Burroughs](https://gc.nh.gov/house/members/member.aspx?member=408822) (D):  PO Box 487  Glen,  NH  03838-0487
 - [Chris R McAleer](https://gc.nh.gov/house/members/member.aspx?member=408973) (D):  PO Box 74  Jackson,  NH  03846-0074
 
-**District 1 - Conway (Not my reps)**
+##### District 1
+
+_**Note:** Jackson is not in District 1, but it is worth noting the representatives from our neighboring town of Conway._
 
 - [Thomas L Buco](https://gc.nh.gov/house/members/member.aspx?member=376645):  PO Box 3149  Conway,  NH  03818-3149
 - [David Paige](https://gc.nh.gov/house/members/member.aspx?member=409144):  State House–House Member Mail 107 North Main Street  Concord,  NH  03301
 - [Stephen L Woodcock](https://gc.nh.gov/house/members/member.aspx?member=408824):  PO Box 234  Center Conway,  NH  03813-0234
 
-**Map**
+**Map of All Counties & Towns in New Hampshire**
+
+![Town & County](/assets/town_count.png)
+
+#### State Senate
+
+##### District 3
+
+The New Hampshire Senate has 24 members, apportioned by population. Jackson is in District 3.
+
+- [Senator McConkey](https://gc.nh.gov/Senate/members/webpages/district03.aspx) (R) State HouseRoom 107  107 North Main Street Concord NH  03301
+
+**Map of State Senate Districts in New Hampshire**
+
+![State Senate Districts](/assets/state_senate.png)
+
+**Map of State House Districts in New Hampshire**
 
 ![State House Districts](/assets/state_house_dist.png)
 
-#### State Senate (24)
+`;
 
-**Names**
-
-- (3) [Senator McConkey](https://gc.nh.gov/Senate/members/webpages/district03.aspx)  (R) State HouseRoom 107  107 North Main Street Concord NH  03301
-
-**Map**
-
-![State Senate Districts](/assets/state_senate.png)
-`);
-
-  // Set the innerHTML of the content div to the converted HTML
+window.onload = function () {
+  const html = marked.parse(markdown);
   document.getElementById("content").innerHTML = html;
 };
